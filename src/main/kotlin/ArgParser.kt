e8e71787793c71b138d250ff3bbbe99c1f282c52
wetrolam <wetrolam@gmail.com>
2018-09-08 23:45:42 +0200

    Add an argument parser

---
+++ src/main/kotlin/ArgParser.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+class ArgParserException(message: String) : Exception(message) {
+}
+
+class ArgParser(val args: Array<String>) {
+
+    fun getBasePath(): File {
+        if (args.size == 1) {
+            val baseDir: File = File(args[0])
+            if (!baseDir.isDirectory()) {
+                throw ArgParserException("\"${baseDir.absolutePath}\" is not a directory")
+            }
+            return baseDir
+        } else {
+            throw ArgParserException("Incorrect number of arguments (set one parameter containing a base project directory)")
+        }
+    }
+}
+
+
--- src/main/kotlin/WebBuilder.kt
+++ src/main/kotlin/WebBuilder.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
-
+    try {
+        val arg: ArgParser = ArgParser(args)
+        println("---- base directory: \"${arg.getBasePath()}\" ----")
+    }
+    catch (e: ArgParserException) {
+        System.err.println(e.message)
+    }
 }
