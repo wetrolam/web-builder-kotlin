26799081e081494e1bd2a27e374729779d3e4c4c
wetrolam <wetrolam@gmail.com>
2018-09-09 11:20:59 +0200

    Add a cleaning of an output directory ("dist") in a user project

--- src/main/kotlin/WebBuilder.kt
+++ src/main/kotlin/WebBuilder.kt
@@ -3,8 +3,16 @@ fun main(args: Array<String>) {
         val arg: ArgParser = ArgParser(args)
         val config: Config = Config(arg.getBasePath())
         println("---- base directory: \"${config.baseDir}\" ----")
+
+        cleanDistributionDirectory(config)
     }
     catch (e: ArgParserException) {
         System.err.println(e.message)
     }
+}
+
+// Create a fresh distribution directory (an output directory in a user web project)
+fun cleanDistributionDirectory(config: Config) {
+    config.distDir.deleteRecursively()
+    config.distDir.mkdir()
 }
