ca3d115c267592ab2de9e389ff230b099eba83a9
wetrolam <wetrolam@gmail.com>
2018-09-14 16:37:10 +0200

    Allow changing a distribution file name

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -6,9 +6,22 @@ class Config(val baseDir: File) {
     val distDir: File = baseDir.resolve("dist")
     val configDir: File = baseDir.resolve("config")
 
-    fun distFileOf(srcFile: File): File {
-        val relativeFilePath: File = srcFile.relativeTo(srcDir)
+    /**
+     * Return distribution version of 'srcFile'.
+     * The returned file has changed path (...../src/..... -> ...../dist/.....) and the name (if 'changeFileName' is defined).
+     * @param srcFile a source file from a user project
+     * @param changeFileName change of the file name of the distribution version
+     * @return distribution version of 'srcFile'
+     */
+    fun distFileOf(srcFile: File, changeFileName: ((File)->File)? = null): File {
+        var relativeFilePath: File = srcFile.relativeTo(srcDir)
+
+        if(changeFileName != null) {
+            relativeFilePath = changeFileName(relativeFilePath)
+        }
+
         val distFile: File = distDir.resolve(relativeFilePath)
+
         return distFile
     }
 }
