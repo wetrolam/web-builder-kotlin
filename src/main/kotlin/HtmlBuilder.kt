130da84f06c3e6ea4c6865ae110e6c834fb7061e
wetrolam <wetrolam@gmail.com>
2018-09-14 16:51:23 +0200

    Allow adding of <head> content (internal css now) by <body> processing

--- src/main/kotlin/HtmlBuilder.kt
+++ src/main/kotlin/HtmlBuilder.kt
@@ -1,39 +1,54 @@
 import java.io.File
 
+data class HtmlData (
+        val body: String,
+        val internalCss: String? = null
+)
+
 // Builder of html files
 class HtmlBuilder(val config: Config) {
 
     // Html template content
     private val baseText: String = config.configDir.resolve("template.html").readText()
 
-    // Build a html file from 'srcFile'. Use 'op' for processig the input file
-    fun exec(srcFile: File, op: (String)->String) {
+    // Build a html file from 'srcFile'. Use 'bodyTransform' for processig the input file
+    fun exec(srcFile: File, bodyTransform: (Config, String)->HtmlData, changeFileName: ((File)->File)? = null) {
         // read the input file
         val srcText: String = srcFile.readText()
 
         // process and wrap the content of the file
-        val processedText: String = op(srcText)
-        val wrappedText: String = wrapByTemplate(processedText, srcFile.name)
+        val htmlData: HtmlData = bodyTransform(config, srcText)
+        val wrappedText: String = wrapByTemplate(htmlData, srcFile.name)
 
         // write the output file
-        val distFile: File = config.distFileOf(srcFile)
+        val distFile: File = config.distFileOf(srcFile, changeFileName)
         distFile.parentFile.mkdirs()
         distFile.writeText(wrappedText)
     }
 
-    private fun wrapByTemplate(srcText: String, srcFileName: String): String {
+    private fun wrapByTemplate(htmlData: HtmlData, srcFileName: String): String {
         // append the file name to <title>
         val titleRegex: Regex = Regex("(?<=<title>).*(?=</title>)", RegexOption.DOT_MATCHES_ALL)
-        val titleText: String = baseText.replace(titleRegex, "$0 - $srcFileName")
+        val titleAdjusted: String = baseText.replace(titleRegex, "$0 - $srcFileName")
+
+        // add internal <style>
+        val styleAdjusted: String =
+                if(htmlData.internalCss != null) {
+                    val styleRegex: Regex = Regex("(?=</head>)", RegexOption.DOT_MATCHES_ALL)
+                    titleAdjusted.replace(styleRegex, "\n<style>\n${htmlData.internalCss}\n</style>\n")
+                }
+                else {
+                    titleAdjusted
+                }
 
         // fill <body>
         val bodyRegex: Regex = Regex("(?<=<body>).*(?=</body>)", RegexOption.DOT_MATCHES_ALL)
-        val distText: String = titleText.replace(bodyRegex, "\n\n$srcText\n\n")
+        val bodyAdjusted: String = styleAdjusted.replace(bodyRegex, "\n\n${htmlData.body}\n\n")
 
-        return distText
+        return bodyAdjusted
     }
 
-    fun expandMacros(input: String): String{
+    fun expandMacros(config: Config, input: String): HtmlData{
         // <a>.cpp</a> <a>.cpp.html</a>
         val aRegex: Regex = Regex("#A\\((.*?)\\)")
         val output: String = input.replace(aRegex){
@@ -42,6 +57,6 @@ class HtmlBuilder(val config: Config) {
             "<a href=\"${sourceFile}\"> ${sourceFile.name} </a> <a href=\"${htmlFile}\"> (html) </a>"
         }
 
-        return output
+        return HtmlData(body = output)
     }
 }
